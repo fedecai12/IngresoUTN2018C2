a43f7e9120d93d420eb8830ecd6d654e9f116885
fedecai12 <40838159+fedecai12@users.noreply.github.com>
2018-07-11 22:16:40 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -3,7 +3,20 @@ function mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
-
+switch (mesDelAño){
+    case "Enero":
+    alert ("que comiences bien el año!!!");
+    break;
+    case "Marzo":
+    alert ("a clases!!!");
+    break;
+    case "Julio":
+    alert ("empiesa las vaciones!!!");
+    break;
+    case "Diciembre":
+    alert ("Felices fiestas!!!");
+    break;
+}
 
 
 }//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -5,7 +5,13 @@ var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
 	
-	
+	switch (mesDelAño){
+        case "Febrero":
+        alert ("Este mes no tiene mas de 29 días!!! ");
+        break;
+        default:
+        alert ("Este mes tiene mas de 30 días o más!!!");
+    }
 
 
 }//FIN DE LA FUNCIÓN
