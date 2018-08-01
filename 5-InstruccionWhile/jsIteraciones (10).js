1321122da51e95321eea9d5ca0bd581336387a5e
fedecai12 <40838159+fedecai12@users.noreply.github.com>
2018-07-31 22:12:21 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -1,16 +1,55 @@
 function mostrar()
 {
 
-	var contador=0;
-	// declarar variables
+	var promedios;
+	var diferencia;
+	var respuesta;
+	var numero;
+	var positivo;
+	var negativo;
+	var contCeros=0;
+	var contPositivos=0;
+	var contNegativo=0;
+	var pares;
+	var promedioNeg;
+	var promedioPositivos;
+	do{
+		numero=parseInt(prompt("Ingrese un numero"));
+		// Me fijo el signo del numero
+		//El numero puede ser negativo, positivo o ceros
+		if(numero >0){
+			//aca van las lineas de codigo que se ejecutan si el numero es positivo
+			contPositivos++;
+			acumPositivos= acumPositivos+numero;
+       }else if (numero <0){
+		   //aca van las lineas de codigo que se ejecutan si el numero es negativo
+	   }
 	
-	var respuesta='si';
-
-	while(respuesta!='no')
-	{
+	  else{
+		  //aca van las lineas de codigo que se ejecutan si el numero
+	   contCeros++;
+	  }
+	//me fijo si el numero es parse 
+	if(numero %2 ==0){
+		//aca va el codigo que se tiene que ejecutar
+ contPares++;	
+}
+respuesta=prompt("Desea continuar");
+	}while (respuesta1= "no");
+	//Aca se realiza el calculode los promedios y la diferencias
+	if (contNegativos!=0){
+    promedioNeg =acumuNegativos/contNegativos
+	}
+   if (contPositivos!= 0){
+     promedioPositivos=acumuPositivos/contPositivos;
+   }
+	
+diferencia=contNegativo-contPositivos;
+document.write("la suma de los negativos es "+negativo + "<br>");
+document.write("la suma de los negativos "+positivo+"<br>");	
 		
 	
-	}
+
 
 
 
