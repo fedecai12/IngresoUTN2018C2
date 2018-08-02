11889d4b8728e70075cd6e7fea7ec07250349fe9
fedecai12 <40838159+fedecai12@users.noreply.github.com>
2018-08-01 22:08:03 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (8).js
+++ 6-InstruccionFor/jsIteracionesFor (8).js
@@ -1,7 +1,8 @@
 function mostrar()
-{
+{var numero
+var primo
 
-
-
-
+numero=parseInt(prompt("Ingrese un numero"));
+for(i=0;i>numero;i++);
+ if(numero%numero)
 }//FIN DE LA FUNCIÓN
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,39 @@
 function mostrar()
-{
+{var nota;
+var promedio;
+var sexo;
+var acumNotas=0;
+var contVarones=0;
+var sexoNotaBaja;
+var flag=0;
+var notaBaja;
 
+for(var i=0;i<5;i++){
+      
+    nota=parseInt(prompt("Ingrese nota: "));
+    while(nota<0 || nota>10){
+        nota=parseInt(prompt("Reingrese nota: "));
+    }
+    sexo=prompt("Ingrese sexo: ");
+    while(sexo!="f" && sexo!="m"||isNaN(nota)){
+        sexo=prompt("Reingrese sexo: ");
+    } 
+    acumNotas=acumNotas+nota;
+    if(nota<notaBaja||flag==0){
+        notaBaja=nota;
+        sexoNotaBaja=sexo;
+    flag=1;
+    }
+   if(sexo=="m"&&nota>=6){
+       contVarones++;
+   }
 }
+promedio=acumNotas/5;
+
+alert("Promedio: "+promedio+ "\n nota mas baja y sexo: " +notaBaja+sexoNotaBaja+"\n Cantidad de Varones: "+contVarones );
+
+
+  
+}
+
+
